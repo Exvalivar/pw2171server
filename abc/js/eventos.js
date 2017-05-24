99679be459f0ef79bd0fc115d1740e19cbe045c1
exvalivar <varoal07_acm@hotmail.com>
2017-05-24 15:06:26 -0600

    eventos php-pendiente,datosphp valida en prueba

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,4 +1,22 @@
 var iniciaApp = function(){
-	alert("hola Ripardo guapo PD:gracias");
+	//alert("hola Ripardo guapo PD:gracias");
+	var entrar=function(){
+		alert($("#txtUsuario").val());
+		alert($("#txtClave").val());
+	}
+	var teclaUsuario=function(tecla){
+		if(tecla.which==13){
+			$("#txtClave").focus();
+		}
+	}
+	var teclaClave=function(tecla){
+		if(tecla.which==13){
+			entrar();
+		}
+	}
+	//sección de declaración de eventows
+	$("#btnEntrar").on("click",entrar);
+	$("#txtUsuario").on("keypress",teclaUsuario);
+	$("#txtClave").on("keypress",teclaClave)
 }
-$(document).on("ready",iniciaApp);
+$(document).ready(iniciaApp);
